fc3cfd9c3e628148fb924e92685f686f7bdcfa27
Haba Andrei <habaconstantin45@gmail.com>
2025-06-13 23:00:17 +0300

    change function get_settings

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from functools import lru_cache
 
 
 class Settings(BaseSettings):
@@ -6,3 +7,7 @@ class Settings(BaseSettings):
     openai_api_key: str
     model_structure_outputs: str = "gpt-4o-mini"
     model_config = SettingsConfigDict(env_file=".env")
+
+@lru_cache
+def get_settings():
+    return Settings()
