81ecefd2ab7d95c22447e3a029ee3fd7b49a19f5
Haba Andrei <habaconstantin45@gmail.com>
2025-07-05 13:13:52 +0300

    add comment in food analyezer code

--- generators/food_analyzer.py
+++ generators/food_analyzer.py
@@ -7,22 +7,28 @@ openai_client = Openai()
 
 class FoodAnalyzer:
 
+    # Main function that manages all functionalities for analyzing food images
     async def analyze_image(self, image:str):
+        # First, we get the general details of the food
         result_general_details = await self.get_general_details(image)
 
+        # If the image does not contain food, we stop further analysis
         is_food = result_general_details.get('data', {}).get('is_food', False)
         if result_general_details.get('is_resolved', False) == False or is_food == False:
             return result_general_details
 
+        # Extract general details
         food_name = result_general_details.get('data', {}).get('name')
         total_quantity = result_general_details.get('data', {}).get('total_quantity')
         health_score = result_general_details.get('data', {}).get('health_score')
         ingredients = result_general_details.get('data').get('ingredients', {})
 
+        # Get nutrient details for each ingredient
         result_ingredients_details = await self.get_ingredients_details(ingredients)
 
         if result_ingredients_details.get('is_resolved') == False :
             return result_ingredients_details
+
         totals = result_ingredients_details.get('data', {}).get('totals')
         ingredients = result_ingredients_details.get('data', {}).get('ingredients')
 
@@ -38,20 +44,23 @@ class FoodAnalyzer:
             }
         }
 
-
-
-    def find_ingredient(self, list_of_directoris, ingredient_name):
-        for directory in list_of_directoris:
+    # Search for an ingredient by name in a list of ingredient dictionaries
+    def find_ingredient(self, list_of_directories, ingredient_name):
+        for directory in list_of_directories:
             if directory.get('name') == ingredient_name:
                 return directory
 
+    # Calculate the nutrient value based on the given quantity
     def get_quantity_value(self, quantity=0, value_100=0):
         one_item = value_100 / 100
         return int(quantity * one_item)
 
+    # Get nutrient details for a list of ingredients
     async def get_ingredients_details(self, ingredients):
         loop = asyncio.get_running_loop()
         results = []
+
+        # Execute nutrient requests concurrently to save time
         with concurrent.futures.ThreadPoolExecutor(10) as executor:
             tasks = [
                 loop.run_in_executor(executor, self.get_nutrients, ingredient)
@@ -67,6 +76,7 @@ class FoodAnalyzer:
 
         ingredients_details = []
 
+        # Process the responses
         for result in results:
             if result.get('is_resolved') == False:
                 return result
@@ -79,17 +89,20 @@ class FoodAnalyzer:
             carbs_per_100 = data_nutrients.get('carbs')
             fats_per_100 = data_nutrients.get('fats')
 
+            # Calculate nutrient values for the specific quantity
             calories = self.get_quantity_value(quantity=quantity, value_100=calories_per_100)
             protein = self.get_quantity_value(quantity=quantity, value_100=protein_per_100)
             carbs = self.get_quantity_value(quantity=quantity, value_100=carbs_per_100)
             fats = self.get_quantity_value(quantity=quantity, value_100=fats_per_100)
 
+            # Update the total values
             total_calories += calories
             total_protein += protein
             total_carbs += carbs
             total_fats += fats
             total_quantity += quantity
 
+            # Add ingredient details to the list
             ingredients_details.append({
                 'name': ingredient_name,
                 'calories': calories,
@@ -111,35 +124,36 @@ class FoodAnalyzer:
     async def get_general_details(self, image:str):
         result = await openai_client.analyze_image(
             system_prompt='''
-                You are an expert in food analysis. Based on the uploaded image, return the structured nutritional info of the meal based on the given JSON schema.
+                You are an expert in food analysis. Based on the uploaded image, return the structured nutritional info of the meal using the provided JSON schema.
                 For each ingredient, specify the approximate cooking method used (e.g., raw, fried, boiled). Be realistic and avoid guessing if uncertain.
-                You will provide the total quantity of the food, as well as the quantity for each individual ingredient. The sum of all ingredient quantities must equal the total quantity of the food.
-                Important: If the image does not contain food, please return the JSON schema with all properties set to null, except for the is_food property, which should be set to false.
+                Provide the total quantity of the food and the quantity for each ingredient. The sum of all ingredient quantities must equal the total quantity.
+                Important: If the image does not contain food, return the JSON schema with all properties set to null, except for the is_food property, which should be set to false.
             ''',
             user_prompt='''
-                 Here is an image of a meal. Please analyze it and provide structured data based on the given JSON schema.
-                For each ingredient, include its name, estimated quantity in grams, and the cooking method used (one of: raw, boiled, fried, baked, grilled, steamed, roasted).
-                Be as accurate as possible based on the visual cues from the image. Do not invent ingredients that are not visible.
+                Here is an image of a meal. Please analyze it and provide structured data according to the provided JSON schema.
+                For each ingredient, include its name, estimated quantity in grams, and the cooking method used (choose from: raw, boiled, fried, baked, grilled, steamed, roasted).
+                Be as accurate as possible based on the visual cues in the image. Do not invent ingredients that are not visible.
             ''',
             image=image,
             json_schema=FoodGeneralDetails
         )
         return result
 
-
+    # Get the nutrients for a specific ingredient
     def get_nutrients(self, ingredient):
-        full_name = 'Name: ' + ingredient.get('name') + ', preparared: ' + ingredient.get('preparation')
-        result_details =  asyncio.run(openai_client.retry_generate_schema(
+        full_name = 'Name: ' + ingredient.get('name') + ', prepared: ' + ingredient.get('preparation')
+
+        result_details = asyncio.run(openai_client.retry_generate_schema(
             system_prompt = '''
                 You are a nutrition analysis expert. Your task is to return the nutrient composition for 100 grams of a given ingredient.
-                For each ingredient, you must provide values per 100 grams for:
+                For each ingredient, provide values per 100 grams for:
                     - Calories (kcal)
                     - Protein (g)
                     - Carbohydrates (g)
                     - Fat (g)
 
-                For the exact weight of the ingredient provided, estimate the nutritional values using data from authoritative sources (e.g., USDA FoodData Central, EFSA, FAO/INFOODS).
-                Be precise, transparent, and rigorous. Your goal is to deliver the most accurate, scientifically grounded nutritional analysis possible.
+                Use authoritative sources such as USDA FoodData Central, EFSA, and FAO/INFOODS.
+                Be precise, transparent, and rigorous. Your goal is to deliver the most accurate and scientifically grounded nutritional analysis possible.
             ''',
             user_prompt = f'''
                 Here is the ingredient: {full_name}.
@@ -151,5 +165,6 @@ class FoodAnalyzer:
         if result_details.get('is_resolved') == False:
             return result_details
         result_dict = result_details.get('data').dict()
+        # Add the ingredient name to the final response
         result_dict['name'] = ingredient.get('name')
         return {'is_resolved': True, 'data': result_dict}
