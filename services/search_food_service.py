b7410dd4b52a5c098974d3825d3b7c0d5fd5a396
Haba Andrei <habaconstantin45@gmail.com>
2025-07-06 10:46:12 +0300

    update SearchFood with meilisearch

--- services/search_food_service.py
+++ services/search_food_service.py
@@ -1,5 +1,5 @@
-from fuzzywuzzy import process
 import pandas as pd
+from providers.meilisearch_client import Meilisearch
 
 # singleton
 class SearchFood():
@@ -7,6 +7,7 @@ class SearchFood():
     _instance = None
     _choices = []
     _food  = {}
+    meilisearch_client = None
 
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
@@ -25,12 +26,17 @@ class SearchFood():
 
         return cls._instance
 
+    def __init__(self):
+        self.meilisearch_client = Meilisearch()
+
     def search(self, input):
         final_results = []
         try:
-            results = process.extract(input, SearchFood._choices, limit=15)
-            for name in results:
-                final_results.append(name[0])
+            result_search = self.meilisearch_client.search(input)
+            for result in result_search:
+                name = result.get('name')
+                ingredient = SearchFood._food[name]
+                final_results.append(ingredient)
             return {'is_resolved': True, 'data': final_results}
         except Exception as e:
             print(e)
