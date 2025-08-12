42996d9b628341c3eb1659b4d6fe0a1e715b59a9
Haba Andrei <habaconstantin45@gmail.com>
2025-08-12 17:12:19 +0300

    store recipes in meilisearch class

--- providers/meilisearch_client.py
+++ providers/meilisearch_client.py
@@ -1,6 +1,7 @@
 import meilisearch
 from config import get_settings
 import json
+from utils.diverse import to_integer
 
 # A singleton class that wraps Meilisearch functionality
 class Meilisearch:
@@ -24,9 +25,54 @@ class Meilisearch:
         with open(path, 'r', encoding='utf-8') as f:
             return [json.loads(line) for line in f]
 
-    def store_food(self):
+    def store_recipes(self):
         """
-        Reads food names from a jsonl file ('food.jsonl') and stores them in a Meilisearch index.
+        Reads recipes from a jsonl file ('recipes.jsonl') and stores them in a Meilisearch index.
+        If the index already exists, it deletes the existing one first to avoid duplication.
+        """
+        if self.exists_index(get_settings().meilisearch_recipe_index):
+            print('Index exists, deleting it first.')
+            self.delete_index(get_settings().meilisearch_recipe_index)
+
+        data_to_store = []
+        rows = self.read_jsonl_file("food_details/recipes.jsonl")
+        for row in rows:
+            recipe_id = row.get("recipe_id")
+            recipe_name = row.get("recipe_name")
+            recipe_description = row.get("recipe_description")
+            serving = row.get("serving_sizes", {}).get("serving", None)
+            if serving:
+                carbohydrate = to_integer(serving.get("carbohydrate", 0))
+                fat = to_integer(serving.get("fat", 0))
+                protein = to_integer(serving.get("protein", 0))
+                calories = to_integer(serving.get("calories", 0))
+
+            ingredinet_names = []
+            ingredients = row.get("ingredients", {}).get("ingredient", [])
+            for ingredinet in ingredients:
+                name = ingredinet.get("food_name")
+                if name:
+                    ingredinet_names.append(name)
+
+            data_to_store.append({
+                'id': recipe_id,
+                'name': recipe_name,
+                'recipe_description': recipe_description,
+                'carbohydrate': carbohydrate,
+                'fat': fat,
+                'protein': protein,
+                'calories': calories,
+                'ingredinet_names': ingredinet_names
+            })
+        # Add the list of recipes to the Meilisearch index
+        Meilisearch._client.index(get_settings().meilisearch_recipe_index).add_documents(data_to_store)
+        return Meilisearch._client.index(get_settings().meilisearch_recipe_index).update_filterable_attributes([
+            'carbohydrate', 'fat', 'protein', 'calories',
+        ])
+
+    def store_foods(self):
+        """
+        Reads food names from a jsonl file ('foods.jsonl') and stores them in a Meilisearch index.
         If the index already exists, it deletes the existing one first to avoid duplication.
         """
         if self.exists_index(get_settings().meilisearch_food_index):
@@ -48,7 +94,7 @@ class Meilisearch:
                 })
                 continue
             data_to_store.append({'id': food_id, 'name': food_name})
-        # Add the list of ingredients to the Meilisearch index
+        # Add the list of food to the Meilisearch index
         return Meilisearch._client.index(get_settings().meilisearch_food_index).add_documents(data_to_store)
 
     def exists_index(self, index: str):
@@ -92,8 +138,12 @@ class Meilisearch:
         return indexes
 
 
-# store ingredients in index
-# Meilisearch().store_food()
+# store foods in index
+# Meilisearch().store_foods()
+
+# store recipes in index
+# Meilisearch().store_recipes()
+
 # python -m providers.meilisearch_client
 
 # result = Meilisearch().search("Apple")
