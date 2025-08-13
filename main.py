a2e22a8bb22885e4f600135ef608b9888a64dba0
Haba Andrei <habaconstantin45@gmail.com>
2025-08-13 15:23:13 +0300

    add Query as parameter

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from generators import fitness_guide, food_analyzer
 from fastapi.middleware.cors import CORSMiddleware
-from providers.schemas import NutritionRequest, AnalyzeImageRequest, SearchFoodRequest, SearchRecipeRequest
+from providers.schemas import NutritionRequest, AnalyzeImageRequest
 from providers.fatsecret_client import FatSecretAPI
 from services import search_food_service, search_recipe_service
+import json
 
 app = FastAPI()
 fitness_guide = fitness_guide.FitnessGuide()
@@ -42,9 +43,10 @@ async def analyze_image(data: AnalyzeImageRequest):
     return await food_analyzer.analyze_image(data.image)
 
 @app.get("/search-food")
-async def search_food(data: SearchFoodRequest):
-    return await search_food_instance.search(data.input)
+async def search_food(input: str):
+    return await search_food_instance.search(input)
 
 @app.get("/search-recipe")
-async def search_recipe(data: SearchRecipeRequest):
-    return await search_recipe_instance.search(data.input, data.query)
+async def search_recipe(input: str, query: str = Query(...)):
+    query_dict = json.loads(query)
+    return await search_recipe_instance.search(input, query_dict)
