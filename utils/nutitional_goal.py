67f70deca2d5d8e3c049bc9a9ef23c5f9afc6be9
Haba Andrei <habaconstantin45@gmail.com>
2025-08-17 16:16:13 +0300

    nutritional goal

---
+++ utils/nutitional_goal.py
@@ -0,0 +1,234 @@
+nutritional_goals = {
+    "child": {
+        "1-3": {
+            "calories": 1000,
+            "protein_g": 13,
+            "protein_pct": "5-20",
+            "carbohydrate_g": 130,
+            "carbohydrate_pct": "45-65",
+            "fiber_g": 14,
+            "fat_pct": "30-40",
+            "saturated_fat_pct": 10,
+            "linoleic_acid_g": 7,
+            "linolenic_acid_g": 0.7,
+            "minerals": {
+                "calcium_mg": 700, "iron_mg": 7, "magnesium_mg": 80,
+                "phosphorus_mg": 460, "potassium_mg": 3000, "sodium_mg": 1500,
+                "zinc_mg": 3, "copper_mg": 0.34, "manganese_mg": 1.2, "selenium_mcg": 20
+            },
+            "vitamins": {
+                "vitamin_a_mcg_RAE": 300, "vitamin_e_mg": 6, "vitamin_d_IU": 600,
+                "vitamin_c_mg": 15, "thiamin_mg": 0.5, "riboflavin_mg": 0.5, "niacin_mg": 6,
+                "vitamin_b6_mg": 0.5, "vitamin_b12_mcg": 0.9, "choline_mg": 200,
+                "vitamin_k_mcg": 30, "folate_mcg_DFE": 150
+            }
+        },
+        "4-8": {
+            "calories": [1200, 1400, 1600],
+            "protein_g": 19,
+            "protein_pct": "10-30",
+            "carbohydrate_g": 130,
+            "carbohydrate_pct": "45-65",
+            "fiber_g": {"female": 16.8, "male": 19.6},
+            "fat_pct": "25-35",
+            "saturated_fat_pct": 10,
+            "linoleic_acid_g": 10,
+            "linolenic_acid_g": 0.9,
+            "minerals": {
+                "calcium_mg": 1000, "iron_mg": 10, "magnesium_mg": 130,
+                "phosphorus_mg": 500, "potassium_mg": 3800, "sodium_mg": 1900,
+                "zinc_mg": 5, "copper_mg": 0.44, "manganese_mg": 1.5, "selenium_mcg": 30
+            },
+            "vitamins": {
+                "vitamin_a_mcg_RAE": 400, "vitamin_e_mg": 7, "vitamin_d_IU": 600,
+                "vitamin_c_mg": 25, "thiamin_mg": 0.6, "riboflavin_mg": 0.6, "niacin_mg": 8,
+                "vitamin_b6_mg": 0.6, "vitamin_b12_mcg": 1.2, "choline_mg": 250,
+                "vitamin_k_mcg": 55, "folate_mcg_DFE": 200
+            }
+        }
+    },
+    "female": {
+        "9-13": {
+            "calories": 1600,
+            "protein_g": 34, "protein_pct": "10-30",
+            "carbohydrate_g": 130, "carbohydrate_pct": "45-65",
+            "fiber_g": 22.4, "fat_pct": "25-35", "saturated_fat_pct": 10,
+            "linoleic_acid_g": 10, "linolenic_acid_g": 1.0,
+            "minerals": {
+                "calcium_mg": 1300, "iron_mg": 8, "magnesium_mg": 240,
+                "phosphorus_mg": 1250, "potassium_mg": 4500, "sodium_mg": 2200,
+                "zinc_mg": 8, "copper_mg": 0.7, "manganese_mg": 1.6, "selenium_mcg": 40
+            },
+            "vitamins": {
+                "vitamin_a_mcg_RAE": 600, "vitamin_e_mg": 11, "vitamin_d_IU": 600,
+                "vitamin_c_mg": 45, "thiamin_mg": 0.9, "riboflavin_mg": 0.9, "niacin_mg": 12,
+                "vitamin_b6_mg": 1.0, "vitamin_b12_mcg": 1.8, "choline_mg": 375,
+                "vitamin_k_mcg": 60, "folate_mcg_DFE": 300
+            }
+        },
+        "14-18": {
+            "calories": 1800,
+            "protein_g": 46, "protein_pct": "10-30",
+            "carbohydrate_g": 130, "carbohydrate_pct": "45-65",
+            "fiber_g": 25.2, "fat_pct": "25-35", "saturated_fat_pct": 10,
+            "linoleic_acid_g": 11, "linolenic_acid_g": 1.1,
+            "minerals": {
+                "calcium_mg": 1300, "iron_mg": 15, "magnesium_mg": 360,
+                "phosphorus_mg": 1250, "potassium_mg": 4700, "sodium_mg": 2300,
+                "zinc_mg": 9, "copper_mg": 0.89, "manganese_mg": 1.6, "selenium_mcg": 55
+            },
+            "vitamins": {
+                "vitamin_a_mcg_RAE": 700, "vitamin_e_mg": 15, "vitamin_d_IU": 600,
+                "vitamin_c_mg": 65, "thiamin_mg": 1.0, "riboflavin_mg": 1.0, "niacin_mg": 14,
+                "vitamin_b6_mg": 1.2, "vitamin_b12_mcg": 2.4, "choline_mg": 400,
+                "vitamin_k_mcg": 75, "folate_mcg_DFE": 400
+            }
+        },
+        "19-30": {
+            "calories": 2000,
+            "protein_g": 46, "protein_pct": "10-35",
+            "carbohydrate_g": 130, "carbohydrate_pct": "45-65",
+            "fiber_g": 28, "fat_pct": "20-35", "saturated_fat_pct": 10,
+            "linoleic_acid_g": 12, "linolenic_acid_g": 1.1,
+            "minerals": {
+                "calcium_mg": 1000, "iron_mg": 18, "magnesium_mg": 310,
+                "phosphorus_mg": 700, "potassium_mg": 4700, "sodium_mg": 2300,
+                "zinc_mg": 8, "copper_mg": 0.9, "manganese_mg": 1.8, "selenium_mcg": 55
+            },
+            "vitamins": {
+                "vitamin_a_mcg_RAE": 700, "vitamin_e_mg": 15, "vitamin_d_IU": 600,
+                "vitamin_c_mg": 75, "thiamin_mg": 1.1, "riboflavin_mg": 1.1, "niacin_mg": 14,
+                "vitamin_b6_mg": 1.3, "vitamin_b12_mcg": 2.4, "choline_mg": 425,
+                "vitamin_k_mcg": 90, "folate_mcg_DFE": 400
+            }
+        },
+        "31-50": {
+            "calories": 1800,
+            "protein_g": 46, "protein_pct": "10-35",
+            "carbohydrate_g": 130, "carbohydrate_pct": "45-65",
+            "fiber_g": 25.2, "fat_pct": "20-35", "saturated_fat_pct": 10,
+            "linoleic_acid_g": 12, "linolenic_acid_g": 1.1,
+            "minerals": {
+                "calcium_mg": 1000, "iron_mg": 18, "magnesium_mg": 320,
+                "phosphorus_mg": 700, "potassium_mg": 4700, "sodium_mg": 2300,
+                "zinc_mg": 8, "copper_mg": 0.9, "manganese_mg": 1.8, "selenium_mcg": 55
+            },
+            "vitamins": {
+                "vitamin_a_mcg_RAE": 700, "vitamin_e_mg": 15, "vitamin_d_IU": 600,
+                "vitamin_c_mg": 75, "thiamin_mg": 1.1, "riboflavin_mg": 1.1, "niacin_mg": 14,
+                "vitamin_b6_mg": 1.3, "vitamin_b12_mcg": 2.4, "choline_mg": 425,
+                "vitamin_k_mcg": 90, "folate_mcg_DFE": 400
+            }
+        },
+        "51+": {
+            "calories": 1600,
+            "protein_g": 46, "protein_pct": "10-35",
+            "carbohydrate_g": 130, "carbohydrate_pct": "45-65",
+            "fiber_g": 22.4, "fat_pct": "20-35", "saturated_fat_pct": 10,
+            "linoleic_acid_g": 11, "linolenic_acid_g": 1.1,
+            "minerals": {
+                "calcium_mg": 1200, "iron_mg": 8, "magnesium_mg": 320,
+                "phosphorus_mg": 700, "potassium_mg": 4700, "sodium_mg": 2300,
+                "zinc_mg": 8, "copper_mg": 0.9, "manganese_mg": 1.8, "selenium_mcg": 55
+            },
+            "vitamins": {
+                "vitamin_a_mcg_RAE": 700, "vitamin_e_mg": 15, "vitamin_d_IU": 600,
+                "vitamin_c_mg": 75, "thiamin_mg": 1.1, "riboflavin_mg": 1.1, "niacin_mg": 14,
+                "vitamin_b6_mg": 1.5, "vitamin_b12_mcg": 2.4, "choline_mg": 425,
+                "vitamin_k_mcg": 90, "folate_mcg_DFE": 400
+            }
+        }
+    },
+    "male": {
+        "9-13": {
+            "calories": 1800,
+            "protein_g": 34, "protein_pct": "10-30",
+            "carbohydrate_g": 130, "carbohydrate_pct": "45-65",
+            "fiber_g": 25.2, "fat_pct": "25-35", "saturated_fat_pct": 10,
+            "linoleic_acid_g": 12, "linolenic_acid_g": 1.2,
+            "minerals": {
+                "calcium_mg": 1300, "iron_mg": 8, "magnesium_mg": 240,
+                "phosphorus_mg": 1250, "potassium_mg": 4500, "sodium_mg": 2200,
+                "zinc_mg": 8, "copper_mg": 0.7, "manganese_mg": 1.9, "selenium_mcg": 40
+            },
+            "vitamins": {
+                "vitamin_a_mcg_RAE": 600, "vitamin_e_mg": 11, "vitamin_d_IU": 600,
+                "vitamin_c_mg": 45, "thiamin_mg": 0.9, "riboflavin_mg": 0.9, "niacin_mg": 12,
+                "vitamin_b6_mg": 1.0, "vitamin_b12_mcg": 1.8, "choline_mg": 375,
+                "vitamin_k_mcg": 60, "folate_mcg_DFE": 300
+            }
+        },
+        "14-18": {
+            "calories": [2200, 2800, 3200],
+            "protein_g": 52, "protein_pct": "10-30",
+            "carbohydrate_g": 130, "carbohydrate_pct": "45-65",
+            "fiber_g": 30.8, "fat_pct": "25-35", "saturated_fat_pct": 10,
+            "linoleic_acid_g": 16, "linolenic_acid_g": 1.6,
+            "minerals": {
+                "calcium_mg": 1300, "iron_mg": 11, "magnesium_mg": 410,
+                "phosphorus_mg": 1250, "potassium_mg": 4700, "sodium_mg": 2300,
+                "zinc_mg": 11, "copper_mg": 0.89, "manganese_mg": 2.2, "selenium_mcg": 55
+            },
+            "vitamins": {
+                "vitamin_a_mcg_RAE": 900, "vitamin_e_mg": 15, "vitamin_d_IU": 600,
+                "vitamin_c_mg": 75, "thiamin_mg": 1.2, "riboflavin_mg": 1.3, "niacin_mg": 16,
+                "vitamin_b6_mg": 1.3, "vitamin_b12_mcg": 2.4, "choline_mg": 550,
+                "vitamin_k_mcg": 75, "folate_mcg_DFE": 400
+            }
+        },
+        "19-30": {
+            "calories": [2400, 2600, 3000],
+            "protein_g": 56, "protein_pct": "10-35",
+            "carbohydrate_g": 130, "carbohydrate_pct": "45-65",
+            "fiber_g": 33.6, "fat_pct": "20-35", "saturated_fat_pct": 10,
+            "linoleic_acid_g": 17, "linolenic_acid_g": 1.6,
+            "minerals": {
+                "calcium_mg": 1000, "iron_mg": 8, "magnesium_mg": 400,
+                "phosphorus_mg": 700, "potassium_mg": 4700, "sodium_mg": 2300,
+                "zinc_mg": 11, "copper_mg": 0.9, "manganese_mg": 2.3, "selenium_mcg": 55
+            },
+            "vitamins": {
+                "vitamin_a_mcg_RAE": 900, "vitamin_e_mg": 15, "vitamin_d_IU": 600,
+                "vitamin_c_mg": 90, "thiamin_mg": 1.2, "riboflavin_mg": 1.3, "niacin_mg": 16,
+                "vitamin_b6_mg": 1.3, "vitamin_b12_mcg": 2.4, "choline_mg": 550,
+                "vitamin_k_mcg": 120, "folate_mcg_DFE": 400
+            }
+        },
+        "31-50": {
+            "calories": 2200,
+            "protein_g": 56, "protein_pct": "10-35",
+            "carbohydrate_g": 130, "carbohydrate_pct": "45-65",
+            "fiber_g": 30.8, "fat_pct": "20-35", "saturated_fat_pct": 10,
+            "linoleic_acid_g": 17, "linolenic_acid_g": 1.6,
+            "minerals": {
+                "calcium_mg": 1000, "iron_mg": 8, "magnesium_mg": 420,
+                "phosphorus_mg": 700, "potassium_mg": 4700, "sodium_mg": 2300,
+                "zinc_mg": 11, "copper_mg": 0.9, "manganese_mg": 2.3, "selenium_mcg": 55
+            },
+            "vitamins": {
+                "vitamin_a_mcg_RAE": 900, "vitamin_e_mg": 15, "vitamin_d_IU": 600,
+                "vitamin_c_mg": 90, "thiamin_mg": 1.2, "riboflavin_mg": 1.3, "niacin_mg": 16,
+                "vitamin_b6_mg": 1.3, "vitamin_b12_mcg": 2.4, "choline_mg": 550,
+                "vitamin_k_mcg": 120, "folate_mcg_DFE": 400
+            }
+        },
+        "51+": {
+            "calories": 2000,
+            "protein_g": 56, "protein_pct": "10-35",
+            "carbohydrate_g": 130, "carbohydrate_pct": "45-65",
+            "fiber_g": 28, "fat_pct": "20-35", "saturated_fat_pct": 10,
+            "linoleic_acid_g": 14, "linolenic_acid_g": 1.6,
+            "minerals": {
+                "calcium_mg": 1200, "iron_mg": 8, "magnesium_mg": 420,
+                "phosphorus_mg": 700, "potassium_mg": 4700, "sodium_mg": 2300,
+                "zinc_mg": 11, "copper_mg": 0.9, "manganese_mg": 2.3, "selenium_mcg": 55
+            },
+            "vitamins": {
+                "vitamin_a_mcg_RAE": 900, "vitamin_e_mg": 15, "vitamin_d_IU": 600,
+                "vitamin_c_mg": 90, "thiamin_mg": 1.2, "riboflavin_mg": 1.3, "niacin_mg": 16,
+                "vitamin_b6_mg": 1.7, "vitamin_b12_mcg": 2.4, "choline_mg": 550,
+                "vitamin_k_mcg": 120, "folate_mcg_DFE": 400
+            }
+        }
+    }
+}
